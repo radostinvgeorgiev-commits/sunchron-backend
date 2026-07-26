13d74d92f4241e1b93f7b22190691738926d7260
radostinvgeorgiev-commits <radostin.v.georgiev@gmail.com>
2026-07-26 23:39:45 +0300

    Добавяне на SYNCHRON-X v3 архитектурна основа

---
+++ tests/toolRegistry.test.js
@@ -0,0 +1,32 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import {
+  findToolsByCapability,
+  getTool,
+  listTools,
+  registerCoreTools,
+  registerTool,
+  resetToolRegistryForTests,
+} from "../src/tools/toolRegistry.js";
+
+test.beforeEach(() => resetToolRegistryForTests());
+
+test("регистрира съществуващите интеграции с пълни метаданни", () => {
+  registerCoreTools();
+  assert.equal(listTools().length, 6);
+  assert.deepEqual(getTool("github-read").capabilities, [
+    "code.read",
+    "code.search",
+    "commit.read",
+  ]);
+});
+
+test("намира активен и здрав инструмент по способност", () => {
+  registerCoreTools();
+  assert.equal(findToolsByCapability("calendar.read")[0].id, "google-calendar-read");
+});
+
+test("не допуска дублиран id", () => {
+  registerCoreTools();
+  assert.throws(() => registerTool(getTool("github-read")), /дублиран id/u);
+});

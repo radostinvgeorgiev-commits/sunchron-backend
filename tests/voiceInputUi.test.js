a7101c6b2512862ce8fe7b362d785dc9a3eae828
radostinvgeorgiev-commits <radostin.v.georgiev@gmail.com>
2026-07-26 13:58:53 +0300

    Add Bulgarian voice input
    * Add Bulgarian voice input
    
    * Add Bulgarian voice input
    
    * Add Bulgarian voice input
    
    * Test Bulgarian voice input

---
+++ tests/voiceInputUi.test.js
@@ -0,0 +1,15 @@
+import assert from "node:assert/strict";
+import { readFile } from "node:fs/promises";
+import test from "node:test";
+
+test("application supports Bulgarian browser voice input", async () => {
+  const [html, script] = await Promise.all([
+    readFile(new URL("../public/index.html", import.meta.url), "utf8"),
+    readFile(new URL("../public/app.js", import.meta.url), "utf8"),
+  ]);
+
+  assert.match(html, /id="voiceBtn"/u);
+  assert.match(script, /webkitSpeechRecognition/u);
+  assert.match(script, /recognition\.lang = 'bg-BG'/u);
+  assert.match(script, /toggleVoiceInput/u);
+});
